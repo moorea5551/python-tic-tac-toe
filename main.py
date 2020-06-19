8ae331eff9a994c9c413730096c4da16a97e7828
Andrew Moore <Andrew.Moore@navihealth.com>
2020-06-18 19:06:26 -0500

    cleaning up the display - had to switch to storing strings in the array instead of ints

--- main.py
+++ main.py
@@ -1,57 +1,9 @@
 import sys
 
-def one():
-    max = 1000
-    multiples = []
-
-    for i in range(1, max):
-        if (i % 3 == 0 or i % 5 == 0):
-            multiples.append(i)
-            print(i)
-
-    sum = 0
-    for j in multiples:
-        sum = sum + j
-
-    print(sum)
-
-def two():
-    max = 4000000
-    fibonacciSeries = [1, 1]
-
-    key = 1
-    sum = 0
-    while fibonacciSeries[key] <= max:
-        nextNumber = fibonacciSeries[key-1] + fibonacciSeries[key]
-        fibonacciSeries.append(nextNumber)
-
-        if nextNumber % 2 == 0:
-            sum = sum + nextNumber
-
-        key+=1
-
-    print(fibonacciSeries)
-    print(sum)
-
-def three():
-    n = 600851475143
-    primeFactors = []
-    i = 2
-
-    while n > 1:
-        if n % i == 0:
-            factors.append(i)
-            n //= i
-            i -= 1
-        i += 1
-    
-    print(primeFactors)
-
 def main():
-    board = [[0, 0, 0],
-             [0, 0, 0],
-             [0, 0, 0]]
-    print(board)
+    board = [[" ", " ", " "],
+             [" ", " ", " "],
+             [" ", " ", " "]]
 
     while True:
         i = 0
@@ -67,18 +19,24 @@ def main():
             print("Enter Column Number: ")
             col = int(input())
 
-            if (board[row][col] == 0):
-                if (player == "X"):
-                    board[row][col] = 1
-                else:
-                    board[row][col] = 2
+            if (board[row][col] == " "):
+                board[row][col] = player
                 i+=1
             else:
                 print("This slot is taken fool!")
 
+            printBoard(board)
 
-            print(board)
-
+def printBoard(board):
+    i = 0
+    print("===========")
+    for row in board:
+        print(row[0] + " | " + row[1] + " | " + row[2])
+        
+        if (i < 2):
+            print("- + - + -")
+        i+=1
+    print("===========")
 
         
         
